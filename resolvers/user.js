55c6fd6dc2b5357f16b39de5e2a77c26e4db0129
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-09-12 19:17:16 +0200

    changed create user to register in resolvers, added encryption for password

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,20 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        models.User.create({ password: encryptedPassword, ...otherArgs });
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    },
   },
 };
